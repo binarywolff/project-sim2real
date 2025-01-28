a422cb5d82c298c91b3bedf307d4c1d992c05006
Fabio Veroli <fabio.veroli@studio.unibo.it>
2025-01-28 17:52:02 +0100

    final version, fix parameters

--- train_cheetah_adr.py
+++ train_cheetah_adr.py
@@ -14,10 +14,10 @@ if __name__ == "__main__":
 
     #Initial ADR parameters
     min_max_bounds = [(1, 10) for _ in env.get_parameters()]
-    masses_bounds = [(0.5 * mass, 1.5 * mass) for mass in env.get_parameters()] #Initial range between 50% and 150% of original masses
-    thresholds = (700, 1600)
-    delta = 0.01 #Update step size 
-    m = 20 #Buffer size
+    masses_bounds = [(0.95 * mass, 1.05 * mass) for mass in env.get_parameters()] 
+    thresholds = (600, 1450)
+    delta = 0.1 #Update step size 
+    m = 1 #Buffer size
     fixed_torso_mass = env.get_parameters()[0]
     
     env = Monitor(env, log_dir)
@@ -31,5 +31,5 @@ if __name__ == "__main__":
     model = SAC("MlpPolicy", env, verbose=0)
     model.learn(total_timesteps=200_000, callback=adr_callback, progress_bar=True)
     model.save(f"models/SAC_ADR_{env_id}")
-    plot_train_results(log_dir, env_id, save_path_figure="training_results/SAC_ADR_")
+    plot_train_results(log_dir, env_id)
     
